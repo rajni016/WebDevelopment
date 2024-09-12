b034fa8625ad37fca28fbd9cdce3f230bd27dc87
Rajini <rajnimaddula88@gmail.com>
2024-09-12 18:39:27 -0400

    Youtube API Call

---
+++ Projects/dependencies_check.js
@@ -0,0 +1,13 @@
+
+
+var isOdd=require("is-odd");// importing the modue is-odd from node_module 
+
+console.log("3 is odd", isOdd(3));
+console.log("4 is odd", isOdd(4));
+console.log("5 is odd", isOdd(5));
+
+var isEven=require("is-even");
+
+console.log("3 is even", isEven(3));
+console.log("4 is even", isEven(4));
+console.log("5 is even", isEven(5));
